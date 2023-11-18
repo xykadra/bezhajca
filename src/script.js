19c50ad37c513f1948e8076f99e2d8cd6a5c22ae
Mirza <kadric681@gmail.com>
2023-11-18 01:31:10 +0100

    Version3BugFixes

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -7,3 +9,27 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+
+  document.querySelectorAll('.dropdown-content a').forEach(function(anchor) {
+    anchor.addEventListener('click', function(event) {
+// Prevent the default behavior of the anchor tag
+   event.preventDefault();
+
+// Get the data-name attribute value
+   var itemName = anchor.getAttribute('data-name');
+
+// Show a confirmation pop-up
+  var isConfirmed = confirm('Are you sure to contact bezhajca regarding ' + itemName + ' ?');
+
+// Perform further actions based on user's confirmation
+   if (isConfirmed) {
+     window.location.reload();
+     // Your code to handle the submission goes here
+     console.log('Item submitted:', itemName);
+   } else {
+     console.log('Submission cancelled.');
+    }
+   });
+ });
